fd6d1ad2009da659f4414a5b7e5a974d75f4fdf4
Coy Sanders <coymeetsworld@gmail.com>
2017-05-11 13:48:51 -0700

    Fixed style issues

--- week2/assignment/RandomizedQueue.java
+++ week2/assignment/RandomizedQueue.java
@@ -18,7 +18,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
       Construct an empty randomized queue. 
     */
     public RandomizedQueue() {
-        q = (Item[])new Object[1]; //make it bigger by default? can't because of memory constraint 
+        q = (Item[]) new Object[1]; // make it bigger by default? can't because of memory constraint 
         size = 0;
     }
 
@@ -45,7 +45,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     public void enqueue(Item item) {
         if (item == null) throw new java.lang.NullPointerException("Can't add a null value to Deque"); 
         if (q.length == size) {
-           Item[] newQ = (Item[])new Object[size*2];
+           Item[] newQ = (Item[]) new Object[size*2];
            for (int i = 0; i < size; i++) newQ[i] = q[i];
            q = newQ;
            q[size++] = item;
@@ -67,30 +67,30 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     public Item dequeue() {
         if (size == 0) throw new java.util.NoSuchElementException("Deque is empty, cannot remove any further items.");
         int itemNumber = StdRandom.uniform(size)+1; // [1, size]
-        //System.out.println("element to remove (start at 1): " + itemNumber);
+        // System.out.println("element to remove (start at 1): " + itemNumber);
         int count = 0;
         int i = 0;
         while(count < itemNumber) {
             if (q[i] != null) {
-              //System.out.println("q[" + i +"] is not null ("+q[i]+")"); 
+              // System.out.println("q[" + i +"] is not null ("+q[i]+")"); 
               count++;
-            } else { /*System.out.println("q[" + i + "]: is null");*/ }
+            } else { /* System.out.println("q[" + i + "]: is null");*/ }
             if (count != itemNumber) i++;
         }
 
-        //System.out.println("out");
-        //System.out.println("count: " + count);
-        //System.out.println("index to remove: " + i);
+        // System.out.println("out");
+        // System.out.println("count: " + count);
+        // System.out.println("index to remove: " + i);
         Item removedItem = q[i];
         q[i] = null;
         size--;
-        if ((double)size/q.length == 0.25) {
-           Item[] newQ = (Item[])new Object[q.length/4];
-           //System.out.println("newQ length: " + newQ.length);
+        if ((double) size/q.length == 0.25) {
+           Item[] newQ = (Item[]) new Object[q.length/4];
+           // System.out.println("newQ length: " + newQ.length);
            int newQIndex = 0;
            for (i = 0; i < q.length; i++) {
                if (q[i] != null) {
-                  //System.out.println("Adding " + q[i] + " to newQ["+newQIndex+"].");
+                  // System.out.println("Adding " + q[i] + " to newQ["+newQIndex+"].");
                   newQ[newQIndex++] = q[i];
                }
            }
@@ -106,14 +106,14 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     public Item sample() {
         if (size == 0) throw new java.util.NoSuchElementException("Deque is empty, cannot sample it.");
         int itemNumber = StdRandom.uniform(size)+1; // [1, size]
-        //System.out.println("element to remove (start at 1): " + itemNumber);
+        // System.out.println("element to remove (start at 1): " + itemNumber);
         int count = 0;
         int i = 0;
-        while(count < itemNumber) {
+        while (count < itemNumber) {
             if (q[i] != null) {
-              //System.out.println("q[" + i +"] is not null ("+q[i]+")"); 
+              // System.out.println("q[" + i +"] is not null ("+q[i]+")"); 
               count++;
-            } else { /*System.out.println("q[" + i + "]: is null");*/ }
+            } else { /* System.out.println("q[" + i + "]: is null");*/ }
             if (count != itemNumber) i++;
         }
 
@@ -134,13 +134,13 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         private Item[] queue;
         private RandomQueueIterator() {
             i = size;
-            queue = (Item[])new Object[size];
+            queue = (Item[]) new Object[size];
             int count = 0;
-            //System.out.println("Length of arr q: " + q.length);
-            for(int i = 0; i < q.length; i++) {
+            // System.out.println("Length of arr q: " + q.length);
+            for (int i = 0; i < q.length; i++) {
                if (q[i] != null) {
                   queue[count++] = q[i]; // Make a deep copy of the queue
-                  //System.out.println("Adding " + q[i] + " found at q["+i+"]" + " ("+ count + " item to add.)");
+                  // System.out.println("Adding " + q[i] + " found at q["+i+"]" + " ("+ count + " item to add.)");
                }
             }
             StdRandom.shuffle(queue); // Shuffle it
@@ -171,24 +171,24 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     public static void main(String[] args) {
         RandomizedQueue<Integer> rqueue = new RandomizedQueue<Integer>();
         for (int i = 1; i <= 10000; i++) {
-            int rand = StdRandom.uniform(0,10);    
+            int rand = StdRandom.uniform(0, 10);
             if (rand == 9) rqueue.size();
             else if (rand == 8) rqueue.isEmpty();
             else if (rand == 7) {
                 try {
                     rqueue.dequeue();
-                } catch (java.util.NoSuchElementException e) { System.out.println(e); };
+                } catch (java.util.NoSuchElementException e) { System.out.println(e); }
             }
-            else if (rand == 6) rqueue.enqueue(StdRandom.uniform(1,10000));
+            else if (rand == 6) rqueue.enqueue(StdRandom.uniform(1, 10000));
             else {
                 try {
                     rqueue.sample();
-                } catch (java.util.NoSuchElementException e) { System.out.println(e); };
+                } catch (java.util.NoSuchElementException e) { System.out.println(e); }
             }
         }
         System.out.println("Size of queue: " + rqueue.size());
         Iterator<Integer> itr = rqueue.iterator();
-        while(itr.hasNext()) {
+        while (itr.hasNext()) {
            System.out.println(itr.next());
         }
     }
